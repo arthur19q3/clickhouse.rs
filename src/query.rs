55ba0404240eb9925c86f7076f696bde4a404571
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-14 11:09:49 +0800

    ini

--- src/query.rs
+++ src/query.rs
@@ -1,7 +1,6 @@
 use hyper::{header::CONTENT_LENGTH, Body, Method, Request};
 use serde::Deserialize;
-use tokio::task;
-use url::Url; // 用于并发任务
+use url::Url;
 
 use crate::{
     cursor::RowBinaryCursor,
@@ -13,7 +12,6 @@ use crate::{
 };
 
 const MAX_QUERY_LEN_TO_USE_GET: usize = 8192;
-const BUFFER_SIZE: usize = 20000; // 缓冲区大小
 
 #[must_use]
 #[derive(Clone)]
@@ -30,24 +28,26 @@ impl Query {
         }
     }
 
-    /// 绑定值到查询中的下一个 `?`。
+    /// Binds `value` to the next `?` in the query.
     ///
-    /// `value` 必须实现 [`Serialize`](serde::Serialize) 或者是一个 [`Identifier`]，并且会被适当地转义。
+    /// The `value`, which must either implement [`Serialize`](serde::Serialize)
+    /// or be an [`Identifier`], will be appropriately escaped.
     ///
-    /// 警告：这意味着查询中不应有任何额外的 `?`，即使它们在字符串字面量中！
+    /// WARNING: This means that the query must not have any extra `?`, even if
+    /// they are in a string literal!
     pub fn bind(mut self, value: impl Bind) -> Self {
         self.sql.bind_arg(value);
         self
     }
 
-    /// 执行查询。
+    /// Executes the query.
     pub async fn execute(self) -> Result<()> {
         self.do_execute(false)?.finish().await
     }
 
-    /// 执行查询，返回一个 [`RowCursor`] 以获取结果。
+    /// Executes the query, returning a [`RowCursor`] to obtain results.
     ///
-    /// # 示例
+    /// # Example
     ///
     /// ```
     /// # async fn example() -> clickhouse::error::Result<()> {
@@ -74,9 +74,9 @@ impl Query {
         Ok(RowCursor(RowBinaryCursor::new(response)))
     }
 
-    /// 执行查询并仅返回一行。
+    /// Executes the query and returns just a single row.
     ///
-    /// 注意 `T` 必须是拥有所有权的类型。
+    /// Note that `T` must be owned.
     pub async fn fetch_one<T>(self) -> Result<T>
     where
         T: Row + for<'b> Deserialize<'b>,
@@ -88,9 +88,9 @@ impl Query {
         }
     }
 
-    /// 执行查询并返回最多一行。
+    /// Executes the query and returns at most one row.
     ///
-    /// 注意 `T` 必须是拥有所有权的类型。
+    /// Note that `T` must be owned.
     pub async fn fetch_optional<T>(self) -> Result<Option<T>>
     where
         T: Row + for<'b> Deserialize<'b>,
@@ -98,39 +98,19 @@ impl Query {
         self.fetch()?.next().await
     }
 
-    /// 执行查询并返回所有生成的结果，收集到一个 Vec 中。
+    /// Executes the query and returns all the generated results, collected into a Vec.
     ///
-    /// 注意 `T` 必须是拥有所有权的类型。
-    ///
-    /// # 缓冲区的意义
-    ///
-    /// 在并行处理中，缓冲区用于暂存从数据库获取的行数据。通过将数据分块处理，可以提高并发性能，减少处理延迟。
-    /// 这里的 `BUFFER_SIZE` 表示每个缓冲区的大小，即每次并行处理的行数。增大缓冲区大小可以减少并发任务的频率，
-    /// 但同时也会增加内存使用。因此，选择合适的缓冲区大小是优化性能和资源使用的关键。
+    /// Note that `T` must be owned.
     pub async fn fetch_all<T>(self) -> Result<Vec<T>>
     where
-        T: Row + for<'b> Deserialize<'b> + std::marker::Send + 'static,
+        T: Row + for<'b> Deserialize<'b>,
     {
-        let mut cursor = self.fetch::<T>()?;
         let mut result = Vec::new();
-
-        // 使用缓冲区并行获取行数据，并保持顺序
-        let mut buffer = Vec::with_capacity(BUFFER_SIZE);
+        let mut cursor = self.fetch::<T>()?;
 
         while let Some(row) = cursor.next().await? {
-            buffer.push(row);
-
-            if buffer.len() >= BUFFER_SIZE {
-                let chunk = buffer.split_off(0);
-                let chunk_result = task::spawn(async move { chunk })
-                    .await
-                    .map_err(Error::from)?;
-                result.extend(chunk_result);
-            }
+            result.push(row);
         }
-
-        // 获取缓冲区中剩余的行数据
-        result.extend(buffer);
 
         Ok(result)
     }
@@ -195,11 +175,11 @@ impl Query {
     }
 }
 
-/// 一个用于发出行数据的游标。
+/// A cursor that emits rows.
 pub struct RowCursor<T>(RowBinaryCursor<T>);
 
 impl<T> RowCursor<T> {
-    /// 发出下一行数据。
+    /// Emits the next row.
     pub async fn next<'a, 'b: 'a>(&'a mut self) -> Result<Option<T>>
     where
         T: Deserialize<'b>,
